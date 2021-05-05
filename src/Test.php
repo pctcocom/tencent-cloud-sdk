c5641939eb3d45813437d1d82ccf469181aeba62
pctcocom <pctco.com@gmail.com>
2021-05-05 14:01:25 +0800

    Update Test.php

--- src/Test.php
+++ src/Test.php
@@ -1,11 +1,12 @@
 <?php
 namespace Pctco\TencentCloudSdk;
 include 'cos-sdk-v5.phar';
+use Qcloud\Cos\Client;
 class Test{
    public function abc(){
       //新建BosClient
       $bucket = "app-develop-test-1258944909";
-      $cosClient = new \Qcloud\Cos\Client([
+      $cosClient = new Client([
          'region' => "ap-shanghai",
          'schema' => 'https', //协议头部，默认为http
          'credentials'=> [
